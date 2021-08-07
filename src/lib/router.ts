7959fa0e426de518a566676374945fa6fa1d13d4
Otsuka Yuhi <11628469+otsukayuhi@users.noreply.github.com>
2021-08-07 21:03:59 +0900

    MySQLの設定を追加しAPIで取得できるように調整

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,9 +1,24 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mysql from 'mysql';
+
+dotenv.config();
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 const router = express.Router();
 
-router.get('/hello', async (_req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+});
+
+router.get('/items', (_req, res) => {
+  connection.query('select * from items', (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results);
+  });
 });
 
 export default router;
