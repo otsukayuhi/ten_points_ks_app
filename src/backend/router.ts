6bd7059986da16a28c4ab489cf86ec8e0bf20de3
Otsuka Yuhi <11628469+otsukayuhi@users.noreply.github.com>
2021-08-09 06:24:57 +0900

    コネクションプールする形に修正

--- src/backend/router.ts
+++ src/backend/router.ts
@@ -8,25 +8,22 @@ const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 const router = express.Router();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
 });
 
-connection.connect((error) => {
-  if (error) {
-    console.error('error connecting: ' + error.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
-
 router.get('/users', (_req: express.Request, res: express.Response) => {
-  connection.query('select * from users', (error, results: User[]) => {
-    if (error) throw error;
-    res.json(results);
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+
+    connection.query('select * from users', (error, results: User[]) => {
+      if (error) throw error;
+      res.json(results);
+      connection.release();
+    });
   });
 });
 
