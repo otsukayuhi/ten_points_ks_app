b2a5c96ce223ef53a3e2b9569b883d7fcbe105d7
Otsuka Yuhi <11628469+otsukayuhi@users.noreply.github.com>
2021-08-08 20:14:36 +0900

    MySQL への接続が切れるエラーの対応

--- src/backend/router.ts
+++ src/backend/router.ts
@@ -15,6 +15,26 @@ const connection = mysql.createConnection({
   database: DB_NAME,
 });
 
+const handleDisconnect = () => {
+  connection.connect((error) => {
+    if (error) {
+      console.log('ERROR.CONNECTION_DB: ', error);
+      setTimeout(handleDisconnect, 1000);
+    }
+  });
+  connection.on('error', (error: mysql.MysqlError) => {
+    console.log('ERROR.DB: ', error);
+    if (error.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.log('ERROR.CONNECTION_LOST: ', error);
+      handleDisconnect();
+    } else {
+      throw error;
+    }
+  });
+};
+
+handleDisconnect();
+
 router.get('/users', (_req: express.Request, res: express.Response) => {
   connection.query('select * from users', (error, results: User[]) => {
     if (error) throw error;
