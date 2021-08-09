a7a664b218be695e4985cd890726848df0d21106
Otsuka Yuhi <11628469+otsukayuhi@users.noreply.github.com>
2021-08-10 00:51:46 +0900

    非同期通信処理を React Query に変更

--- src/frontend/App/index.tsx
+++ src/frontend/App/index.tsx
@@ -1,35 +1,25 @@
 import { getUsers } from 'frontend/gateway/getUsers';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useQuery } from 'react-query';
 import { User } from 'types/user';
 
 import styles from './index.module.css';
 
 export const App: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isError, setIsError] = useState<boolean>(false);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const users = await getUsers();
-        setUsers(users);
-      } catch (error) {
-        console.log(error);
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    })();
-  }, []);
+  const {
+    data = [],
+    isError,
+    error,
+    isLoading,
+  } = useQuery<User[], Error>('users', getUsers);
 
   if (isLoading) return <div>loading...</div>;
 
-  if (isError) return <div>取得に失敗しました</div>;
+  if (isError) return <div>取得に失敗しました。Error: {error?.message}</div>;
 
   return (
     <ul className={styles.App}>
-      {users.map((user) => (
+      {data.map((user) => (
         <li key={user.id}>{user.name}</li>
       ))}
     </ul>
--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -4,12 +4,17 @@ import './globals.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { App } from './App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.querySelector('#root')
 );
