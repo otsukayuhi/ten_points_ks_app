00ed0cc7785018ff9ac462ba263482cc9bdfe502
Yuhi Otsuka <y_otsuka@yumemi.co.jp>
2021-08-16 22:04:37 +0900

    SQL 部分を定数に変更

--- src/backend/repository/getUsers.ts
+++ src/backend/repository/getUsers.ts
@@ -3,13 +3,14 @@ import mysql from 'mysql';
 import { User } from 'types/user';
 
 const pool = mysql.createPool(db_config);
+const sql = 'select * from users';
 
 export const getUsers = async (): Promise<User[]> => {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, connection) => {
       if (err) reject(err);
 
-      connection.query('select * from users', (error, results: User[]) => {
+      connection.query(sql, (error, results: User[]) => {
         if (error) reject(error);
         resolve(results);
         connection.release();
