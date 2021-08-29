e841059b0724a3e5a32d6174434a3af34e2398ed
Otsuka Yuhi <11628469+otsukayuhi@users.noreply.github.com>
2021-08-29 22:36:59 +0900

    JSDoc を修正

--- src/backend/application/UserApplicationService.ts
+++ src/backend/application/UserApplicationService.ts
@@ -29,7 +29,7 @@ export class UserApplicationService {
   }
 
   /**
-   * getUsers
+   * getUserList
    */
   public async findAll(): Promise<UserModel[]> {
     const userList = await this.userRepository.findAll();
